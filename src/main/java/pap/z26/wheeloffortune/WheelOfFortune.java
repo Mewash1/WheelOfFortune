5cba2526d8e823a1641052afbaa355cc5deccdb5
b1rtek <skrzeczek@onet.pl>
2022-11-29 09:45:48 +0100

    ShadowJar dodany do build.gradle

--- src/main/java/pap/z26/wheeloffortune/WheelOfFortune.java
+++ src/main/java/pap/z26/wheeloffortune/WheelOfFortune.java
@@ -2,6 +2,6 @@ package pap.z26.wheeloffortune;
 
 public class WheelOfFortune {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("PAP Z26");
     }
 }
