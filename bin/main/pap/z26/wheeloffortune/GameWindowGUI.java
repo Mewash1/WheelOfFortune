5bbfd4c6b54e71357f9f769d15800fc9c5066407
mwroblew <01171701@pw.edu.pl>
2022-12-03 19:54:54 +0100

    gui 0.2

--- bin/main/pap/z26/wheeloffortune/GameWindowGUI.java
+++ bin/main/pap/z26/wheeloffortune/GameWindowGUI.java
@@ -4,23 +4,27 @@ public class GameWindowGUI {
     private JButton newGameButton;
     private JProgressBar RoundProgress;
     private JButton guessLetter;
-    private JButton ALLINGUESSFULLButton;
+    private JButton fullGuess;
     private JTable playersScores;
     private JButton helpButton;
-    private JList guessesHistory;
-    private JPanel buttonsPanel;
-    private JTextField pricePool;
-    private JTextField currentPlayer;
-    private JPasswordField playerInput;
+    private JLabel pricePool;
+    private JLabel currentPlayer;
+    private JTextField playerInput;
     private JTextField roundSollution;
     private JPanel mainPannel;
     private JPanel leftMenu;
     private JPanel rightMenu;
-    private JPanel inputPannel;
     private JToolBar topToolBar;
     private JSplitPane mainDivider;
     private JSplitPane leftMenuVertDivider;
-    private JToolBar priceAndPlayerBar;
-    private JToolBar guessButtonsBar;
+    private JList guessesHistory;
+    private JScrollPane guessLog;
+    private JPanel topPanel;
+    private JPanel bottomPanel;
+    private JCheckBox visibleCheckBox;
+    private JLabel roundNr3Label;
 
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
