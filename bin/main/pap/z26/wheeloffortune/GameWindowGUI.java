23a0a830fb5c027c76647f5f47f1f9845ad7e6a2
mwroblew <01171701@pw.edu.pl>
2022-12-03 19:00:41 +0100

    refactor to (...)GUI renamed all components

---
+++ bin/main/pap/z26/wheeloffortune/GameWindowGUI.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+public class GameWindowGUI {
+    private JButton newGameButton;
+    private JProgressBar RoundProgress;
+    private JButton guessLetter;
+    private JButton ALLINGUESSFULLButton;
+    private JTable playersScores;
+    private JButton helpButton;
+    private JList guessesHistory;
+    private JPanel buttonsPanel;
+    private JTextField pricePool;
+    private JTextField currentPlayer;
+    private JPasswordField playerInput;
+    private JTextField roundSollution;
+    private JPanel mainPannel;
+    private JPanel leftMenu;
+    private JPanel rightMenu;
+    private JPanel inputPannel;
+    private JToolBar topToolBar;
+    private JSplitPane mainDivider;
+    private JSplitPane leftMenuVertDivider;
+    private JToolBar priceAndPlayerBar;
+    private JToolBar guessButtonsBar;
+
+}
