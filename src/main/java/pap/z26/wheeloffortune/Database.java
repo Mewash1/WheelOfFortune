44dcbffce6335cb455efaeaa208669f610757dfd
Miloszun <mizak.milosz@gmail.com>
2022-12-03 17:40:24 +0100

    utworzono klasę DatabaseCommand, dodano metodę InsertPhrases do klasy Database

--- src/main/java/pap/z26/wheeloffortune/Database.java
+++ src/main/java/pap/z26/wheeloffortune/Database.java
@@ -6,66 +6,15 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-
 public class Database {
 
     private static volatile Database instance;
 
     private Connection connection;
 
-    public Database() {
-        try {
-            Class.forName("org.sqlite.JDBC");
-        } catch (ClassNotFoundException e) {
-            System.err.println("Missing SQLite JDBC driver");
-            e.printStackTrace();
-            System.exit(1);
-        }
-
-        try {
-            connection = DriverManager.getConnection("jdbc:sqlite:wofDatabase.db");
-            Statement stmt = connection.createStatement();
-
-            try{
-                String sql_Phrases = """
-                    CREATE TABLE Phrases(
-                        PhraseID INTEGER PRIMARY KEY,
-                        Content TEXT NOT NULL,
-                        Category TEXT NOT NULL
-                    )""";
-                stmt.execute(sql_Phrases);
-            } catch (SQLException ignored) {}
-
-            try{
-                String sql_HighScores = """
-                    CREATE TABLE HighScores(
-                        PlayerID INTEGER PRIMARY KEY,
-                        Score INTEGER NOT NULL,
-                        AchievedOn DATE NOT NULL
-                    )
-                    """;
-                stmt.execute(sql_HighScores);
-            } catch (SQLException ignored) {}
-
-            try {
-                String sql_PhrasesToHighScores = """
-                    CREATE TABLE PhrasesToHighScores(
-                        PhsID INTEGER PRIMARY KEY,
-                        PlayerID INTEGER NOT NULL,
-                        PhraseID INTEGER NOT NULL,
-                        CONSTRAINT fk_highscores FOREIGN KEY (PlayerID)
-                            REFERENCES HighScores(PlayerID),
-                        CONSTRAINT fk_phrases FOREIGN KEY (PhraseID)
-                            REFERENCES Phrases(PhraseID)
-                    )
-                    """;
-                stmt.execute(sql_PhrasesToHighScores);
-            } catch (SQLException ignored) {}
-        } catch (SQLException e) {
-            System.err.println("Couldn't connect to wofDatabase.db");
-            e.printStackTrace();
-            System.exit(1);
-        }
+    private Database(){
+        DatabaseCommand.callCommand(new DatabaseCommand.CreateTables(), establishConnection());
+        this.insertPhrases();
     }
 
     public static Database getInstance() {
@@ -81,6 +30,28 @@ public class Database {
         }
     }
 
+    private Statement establishConnection(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            System.err.println("Missing SQLite JDBC driver");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:wofDatabase.db");
+            return connection.createStatement();
+        } catch (SQLException e) {
+            System.err.println("Couldn't connect to wofDatabase.db");
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return null;
+    }
+    private void insertPhrases() {
+        DatabaseCommand.callCommand(new DatabaseCommand.InsertPhrases(), establishConnection());
+    }
     public String getRandomPhrase(String category) {
         return null;
     }
@@ -99,9 +70,5 @@ public class Database {
 
     public boolean updateDatabase() {
         return false; // to na później
-    }
-
-    public static void main(String[] args){
-        Database database = new Database();
     }
 }
