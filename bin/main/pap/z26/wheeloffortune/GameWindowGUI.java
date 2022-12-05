c933d723f0ff5dc38c9255c89699f1c7cb026c44
mwroblew <01171701@pw.edu.pl>
2022-12-05 22:26:39 +0100

    listyteners added.

--- bin/main/pap/z26/wheeloffortune/GameWindowGUI.java
+++ bin/main/pap/z26/wheeloffortune/GameWindowGUI.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GameWindowGUI extends JFrame {
     private JButton newGameButton;
@@ -9,7 +11,7 @@ public class GameWindowGUI extends JFrame {
     private JButton helpButton;
     private JLabel pricePool;
     private JLabel currentPlayer;
-    private JTextField playerInput;
+    private JPasswordField playerInput;
     private JTextField roundSollution;
     private JPanel mainPannel;
     private JPanel leftMenu;
@@ -27,10 +29,56 @@ public class GameWindowGUI extends JFrame {
     private JButton spinWheelButton;
 
     public GameWindowGUI() {
+        setContentPane(mainPannel;
         setTitle("WheelOfFortune");
         setSize(450, 300);
         setDefaultCloseOperation((WindowConstants.EXIT_ON_CLOSE));
         setVisible(true);
+        guessLetter.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(playerInput.getText());
+            }
+        });
+        fullGuess.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(playerInput.getText());
+            }
+        });
+        visibleCheckBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JCheckBox c = (JCheckBox) e.getSource();
+                playerInput.setEchoChar(c.isSelected() ? '\u0000' : (char) 0);
+            }
+        });
+        spinWheelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Brryyyyt");
+            }
+        });
+        newGameButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("We are rich");
+            }
+        });
+        helpButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Nope");
+
+            }
+        });
+        addPlayerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Hello player nb 2");
+
+            }
+        });
     }
 
     public static void main(String[] args) {
