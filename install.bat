7093756a521d71755acb0585dbb22f1f3dd87e62
B1rtek <53182944+B1rtek@users.noreply.github.com>
2022-12-07 19:17:43 +0100

    Install scripts fix

--- install.bat
+++ install.bat
@@ -1,4 +1,2 @@
-git clone https://gitlab-stud.elka.pw.edu.pl/wtopolsk/PAP22Z-Z26
-cd PAP22Z-Z26
 gradle build
 cp build/libs/WheelOfFortune-1.0-PROTOTYPE-all.jar WheelOfFortune.jar
--- install.sh
+++ install.sh
@@ -1,4 +1,2 @@
-git clone https://gitlab-stud.elka.pw.edu.pl/wtopolsk/PAP22Z-Z26
-cd PAP22Z-Z26 || exit
 gradle build
 cp build/libs/WheelOfFortune-1.0-PROTOTYPE-all.jar WheelOfFortune.jar
