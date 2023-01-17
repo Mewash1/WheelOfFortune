beadd35805baca33dfdb8777e87aabb408f4bbb1
b1rtek <53182944+B1rtek@users.noreply.github.com>
2023-01-18 00:48:20 +0100

    Added documentation for GameWord and Wheel

--- src/main/java/pap/z26/wheeloffortune/GameWord.java
+++ src/main/java/pap/z26/wheeloffortune/GameWord.java
@@ -12,6 +12,7 @@ public class GameWord {
     public static ArrayList<Character> consonants, vowels, letters;
 
     static {
+        // initializes the character arrays
         consonants = new ArrayList<>();
         vowels = new ArrayList<>();
         String consonantsString = "qwrtpsdfghjklzxcvbnmćżźńłś", vowelsString = "aeiouyąęó";
@@ -25,6 +26,11 @@ public class GameWord {
         letters.addAll(vowels);
     }
 
+    /**
+     * Initializes the GameWord with a new phrase
+     *
+     * @param word phrase to guess in the game
+     */
     public GameWord(String word) {
         this.word = word.toLowerCase(Locale.ROOT);
         this.notGuessedWord = word.toLowerCase(Locale.ROOT);
@@ -37,6 +43,13 @@ public class GameWord {
         displayedWord = displayedWordBuilder.toString();
     }
 
+    /**
+     * Checks how many times the given letter occurs in the uncovered part of the phrase and returns that amount, used
+     * when a {@link Player player} guesses a letter in game
+     *
+     * @param letter letter to check the count of in the uncovered part of the phrase
+     * @return count of this letter in the uncovered part of the phrase
+     */
     public int guessLetter(char letter) {
         int count = 0;
         if (notGuessedWord.contains(String.valueOf(letter))) {
@@ -53,6 +66,12 @@ public class GameWord {
         return count;
     }
 
+    /**
+     * Checks whether the provided phrase matches the one saved in this GameWord
+     *
+     * @param phrase phrase that a {@link Player player} guessed
+     * @return true if the given phrase matches the solution, false otherwise
+     */
     public boolean guessPhrase(String phrase) {
         return phrase.toLowerCase(Locale.ROOT).equals(word.toLowerCase(Locale.ROOT));
     }
@@ -74,11 +93,16 @@ public class GameWord {
         return !displayedWord.equals(word);
     }
 
+    /**
+     * Uncovers a single random letter. This method is only called in games being run on the {@link GameServer game server}
+     *
+     * @return index of the letter to uncover, or -1 if all are uncovered
+     */
     public int uncoverRandomLetter() {
-        if(!hasNotGuessedLetters()) return -1;
+        if (!hasNotGuessedLetters()) return -1;
         ArrayList<Integer> hiddenLettersIndexes = new ArrayList<>();
-        for(int i=0; i<displayedWord.length(); i++) {
-            if(displayedWord.charAt(i) == '_') hiddenLettersIndexes.add(i);
+        for (int i = 0; i < displayedWord.length(); i++) {
+            if (displayedWord.charAt(i) == '_') hiddenLettersIndexes.add(i);
         }
         Random random = new Random();
         int toUncover = hiddenLettersIndexes.get(random.nextInt(hiddenLettersIndexes.size()));
@@ -86,8 +110,13 @@ public class GameWord {
         return toUncover;
     }
 
+    /**
+     * Uncovers a letter at the given index. This method is only called by games run in the {@link WheelOfFortune game client}
+     *
+     * @param toUncover index of the letter to uncover
+     */
     public void uncoverRandomLetter(int toUncover) {
-        if(toUncover == -1) return;
+        if (toUncover == -1) return;
         StringBuilder uncovered = new StringBuilder(displayedWord);
         uncovered.setCharAt(toUncover, word.charAt(toUncover));
         displayedWord = uncovered.toString();
--- src/main/java/pap/z26/wheeloffortune/Wheel.java
+++ src/main/java/pap/z26/wheeloffortune/Wheel.java
@@ -10,13 +10,23 @@ public class Wheel {
     private int lastRolled = -3, riggedRoll = -174;
     private final Random random = new Random();
 
+    /**
+     * Creates a Wheel object that downloads the wheel contents from the {@link Database Database}
+     */
     public Wheel() {
         Database database = Database.getInstance();
         wheelContents = database.getWheelContents();
     }
 
+    /**
+     * Spins the wheel, returning a random value from the wheel corresponding to the given round, or a rigged value set
+     * by a response from the {@link GameServer game server}
+     *
+     * @param state enum specifying which wheel should be spun, corresponding to the current round number
+     * @return random value from the wheel, or the one set by the server in advance
+     */
     public int spin(GameState state) {
-        if(riggedRoll != -174) { // rigged by server
+        if (riggedRoll != -174) { // rigged by server
             lastRolled = riggedRoll;
             riggedRoll = -174;
         } else {
@@ -25,6 +35,12 @@ public class Wheel {
         return lastRolled;
     }
 
+    /**
+     * Sets a value that will be returned on the next spinning of the wheel, used by {@link GameServer game server} to
+     * simulate a spin done by a player in local games
+     *
+     * @param value value that will be returned on the next spin
+     */
     public void rig(int value) {
         riggedRoll = value;
     }
